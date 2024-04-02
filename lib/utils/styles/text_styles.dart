d44151ec057fe6bdb5f469de11ce841846c7f686
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2024-04-02 23:45:14 +0500

    Complete project configurations

---
+++ lib/utils/styles/text_styles.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:neuro_care_hub_app/utils/extensions/size_extension.dart';
+
+class TextStyles {
+  static TextStyle boldDarkMediumTextStyle() {
+    return TextStyle(
+      fontFamily: 'Poppins',
+      fontSize: 14.0.sp,
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+    );
+  }
+
+  static TextStyle boldDarkLargeTextStyle() {
+    return TextStyle(
+      fontFamily: 'Poppins',
+      fontSize: 16.0.sp,
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+    );
+  }
+
+  static TextStyle boldDarkSmallTextStyle() {
+    return TextStyle(
+      fontFamily: 'Poppins',
+      fontSize: 11.4.sp,
+      color: Colors.black,
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle boldLightSmallTextStyle() {
+    return TextStyle(
+      fontFamily: 'Poppins',
+      fontSize: 10.8.sp,
+      color: Colors.black54,
+      fontWeight: FontWeight.w500,
+    );
+  }
+}
